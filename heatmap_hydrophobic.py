1d342f33cb8842cc3eaa242b5a51810027aefdf4
Hassan Ali <150252797+hass-a-l-i@users.noreply.github.com>
2023-11-25 13:36:59 +0000

    Update heatmap_hydrophobic.py

--- heatmap_hydrophobic.py
+++ heatmap_hydrophobic.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+# prints heatmap of residue - residue contacts vs time in contact on interface of the dimer that formed 
 x, y, z = [], [], []
-#frac_at_10A.txt
+
 with open("frac_at_10A.txt") as f:
     for line in f:
         cols = line.split()
@@ -21,9 +22,8 @@ c = [i * 100 for i in e]
 d = list(map("{}%".format, c))
 print("max frac frame bound =", max(z))
 data = pd.DataFrame(data={'N2 hydrophobic residues': x, 'N3 hydrophobic residues': y, 'Fraction of frames bound': c})
-# data = pd.DataFrame(data={'P2 hydrophobic residues': x, 'P3 hydrophobic residues': y, 'Fraction of frames bound': c})
 data = data.pivot(index='N2 hydrophobic residues', columns='N3 hydrophobic residues', values='Fraction of frames bound')
-# print(data)
+print(data)
 sns.set(font_scale=1)
 svm = sns.heatmap(data, cmap='coolwarm', annot=True, annot_kws={"fontsize":10}, vmax=100, vmin=0, cbar_kws={'label': 'Percentage of $T^{\mathrm{N2, N3}}_{\mathrm{dimer}}$ in contact (%)', 'ticks': [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]},  square=True, linewidths=0.01, linecolor='black')
 svm.invert_yaxis()
