0c37c5d179e387bdaa077cae7723283f2b27e7eb
Hassan Ali <150252797+hass-a-l-i@users.noreply.github.com>
2023-11-25 13:34:31 +0000

    Update and rename correlation.py to time_contacts.py

--- time_contacts.py
+++ time_contacts.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import ticker
 
+# prints the grpah showing correlation of the contacts between different peptide pairs
+# shows the propensity to form dimers
 x, y, z = [], [], []
 
 with open("new_correlation_10A.txt") as f:
@@ -11,12 +13,14 @@ with open("new_correlation_10A.txt") as f:
         if len(cols) == 2:
             x.append(float(cols[0]))
             y.append(float(cols[1]))
+
+# print the graph => output shows average number of contacts vs total percentage of time in contact
+# shows the exponential increase in the time of contact vs number of contacts for any point on the peptide
 x = sorted(x)
 y = sorted(y)
 plt.style.use('seaborn-whitegrid')
 fig = plt.gcf()
 ax1 = fig.add_subplot(111)
-# ax1.set_title("Dimer system : P{a} and P{b} aggregation (all atom)".format(a=peptide_a, b=peptide_b))
 ax1.set_xlabel('Percentage of $T^{\mathrm{N2, N3}}_{\mathrm{dimer}}$ in contact (%)', fontsize=12)
 ax1.set_ylabel("Average number of contacts", fontsize=12)
 x = np. array(x)
@@ -33,8 +37,6 @@ d = y[-3:]
 plt. plot(a, b, 'o', color="tab:blue", label="Bound < 90% of $T^{\mathrm{N2, N3}}_{\mathrm{dimer}}$")
 plt. plot(c, d, 'o', color="tab:red", label="Bound > 90% of $T^{\mathrm{N2, N3}}_{\mathrm{dimer}}$")
 plt.legend(loc="upper left", frameon=True)
-#plt.xlim(0, 70)
-#plt.ylim(0, 1.3)
 for axis in [ax1.xaxis]:
     axis.set_major_locator(ticker.MaxNLocator(prune='lower'))
 fig.savefig("correlation.pdf", bbox_inches='tight')
